4f703728bf641fe7003f4312369afec32e028d90
Etudiant <Etudiant>
2017-03-29 17:17:56 +0200

    Javascript suite

---
+++ 29 mars/exo 10/js/script.js
@@ -0,0 +1,17 @@
+
+// Créer un objet contenant 4 propriétés
+var users={
+    first: 'Nesta',
+    second: 'Bunny',
+    third: 'Peter',
+    fourth:'Lee'
+}
+
+// Faire une boucle FOR..IN sur l'objet
+for( var prop in users){
+    // Afficher le nom de la propriété
+    console.log(prop);
+
+    // Afficher la valeur des propriétés
+    console.log(users[prop]);
+};
